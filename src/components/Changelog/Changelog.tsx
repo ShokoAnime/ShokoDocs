fa9100a5e7d102da5fef7cc85d61a627f1e152dd
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-06-06 02:15:24 -0700

    Updated Changelog component and added remaining data and pages.

--- src/components/Changelog/Changelog.tsx
+++ src/components/Changelog/Changelog.tsx
@@ -1,45 +1,49 @@
 // Imports
-import React from "react";
-import Lodash from "Lodash"
-import { program, typeColor } from "./Changelog.utils"
+import React from 'react'
+import Lodash from 'Lodash'
+import { program, typeColor } from './Changelog.utils'
 
 export default function Changelog(props) {
+  interface releaseInfo {
+    changes: object
+    date: string
+    link: string
+    version: string
+  }
 
-	const releaseInfo = Lodash.map(program(props.name), release => {
-		return {
-			date: release.date,
-			version: release.version,
-			link: release.link,
-			changes: Lodash.groupBy(release.changes, "type")
-		}
-	})
+  const releaseInfo = Lodash.map(program(props.name), ({ changes, date, link, version }: releaseInfo) => {
+    return {
+      changes: Lodash.groupBy(changes, 'type'),
+      date: date,
+      link: link,
+      version: version,
+    }
+  })
 
-	const BuildChanges = (release) => (
-		Lodash.map(release.info.changes, (changeGroupMap, changeGroupKey) => (
-			<div className='changelog-type-wrapper' key={changeGroupKey}>
-				<div className={typeColor(changeGroupKey)}>{changeGroupKey}</div>
-				<div className='changelog-item-wrapper'>
-					{Lodash.map(changeGroupMap, (changeItemMap, changeItemKey) => (
-						<div key={changeItemKey}>- {changeItemMap.text}</div>
-					))}
-				</div>
-			</div>
-		))
-	)
+  const BuildChanges = (release) =>
+    Lodash.map(release.info.changes, (changeGroupMap, changeGroupKey) => (
+      <div className='changelog-type-wrapper' key={changeGroupKey}>
+        <div className={typeColor(changeGroupKey)}>{changeGroupKey}</div>
+        <div className='changelog-item-wrapper'>
+          {Lodash.map(changeGroupMap, (changeItemMap, changeItemKey) => (
+            <div key={changeItemKey}>- {changeItemMap.text}</div>
+          ))}
+        </div>
+      </div>
+    ))
 
-	return (
-		Lodash.map(releaseInfo, (info, infoKey) => (
-			<div className='changelog-wrapper' key={infoKey}>
-				<div className='changelog-header'>
-					<div>Version {info.version}</div>
-					<span>|</span>
-					<div>{info.date}</div>
-					<span>|</span>
-					{info.link !== 'NA' ? <a href={info.link} target='_blank'>View Release Notes</a> : 'Release Notes Not Available'}
-				</div>
-				{BuildChanges({info})}
-			</div>
-		))
-	)
+  return Lodash.map(releaseInfo, (info, infoKey) => (
+    <div className='changelog-wrapper' key={infoKey}>
+      <div className='changelog-left'>{BuildChanges({ info })}</div>
+      <div className='changelog-right'>
+        <div className='changelog-version'>Version {info.version}</div>
+        <div className='changelog-date'>{info.date}</div>
+        {info.link !== 'NA' ? (
+          <a className='changelog-link' href={info.link} target='_blank'>
+            View Release Notes
+          </a>
+        ) : null}
+      </div>
+    </div>
+  ))
 }
-
--- src/components/Changelog/Changelog.utils.tsx
+++ src/components/Changelog/Changelog.utils.tsx
@@ -1,32 +1,37 @@
 // Imports
-import server from "../../data/changelog/server.json";
-import desktop from "../../data/changelog/desktop.json";
+import server from '../../data/changelog/server.json'
+import serverWebUI from '../../data/changelog/webui.json'
+import desktop from '../../data/changelog/desktop.json'
+import shokofin from '../../data/changelog/shokofin.json'
+import shokoMetadata from '../../data/changelog/shokometadata.json'
+import myAnime3 from '../../data/changelog/myanime3.json'
 
 export const program = (programName) => {
-	switch(programName) {
-		case 'Shoko Server':
-			return server.releases
-		case 'Shoko Web UI':
-			return server.releases
-		case 'Shoko Desktop':
-			return desktop.releases
-		case 'Shoko Metadata':
-			return server.releases
-		case 'Shokofin':
-			return server.releases
-	}
+  switch (programName) {
+    case 'Shoko Server':
+      return server.releases
+    case 'Shoko WebUI':
+      return serverWebUI.releases
+    case 'Shoko Desktop':
+      return desktop.releases
+    case 'Shoko Metadata':
+      return shokoMetadata.releases
+    case 'Shokofin':
+      return shokofin.releases
+    case 'My Anime 3':
+      return myAnime3.releases
+  }
 }
 
 export const typeColor = (type) => {
-
-	switch (type) {
-		case 'added':
-			return 'changelog-type changelog-added'
-		case 'changed':
-			return 'changelog-type changelog-changed'
-		case 'fixed':
-			return 'changelog-type changelog-fixed'
-		case 'removed':
-			return 'changelog-type changelog-removed'
-	}
+  switch (type) {
+    case 'added':
+      return 'changelog-type changelog-added'
+    case 'changed':
+      return 'changelog-type changelog-changed'
+    case 'fixed':
+      return 'changelog-type changelog-fixed'
+    case 'removed':
+      return 'changelog-type changelog-removed'
+  }
 }
