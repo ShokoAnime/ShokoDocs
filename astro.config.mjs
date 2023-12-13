6628c800bf812fb438ddaed8b6e8b7bd28d90fef
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-12-13 00:16:49 -0800

    Additional Work on Frame.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'ShokoDocs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/ShokoAnime/ShokoDocs',
-			},
-			lastUpdated: true,
-			sidebar: [
-				{
-					label: 'Getting Started',
-					// Autogenerate a group of links for the 'getting-started' directory.
-					autogenerate: { directory: 'getting-started' },
-				},
-			],
-			components: {
-				TableOfContents: './src/components/TableOfContents.astro',
-			},
-		}),
-	],
+  integrations: [starlight({
+    title: 'ShokoDocs',
+    logo: {
+      light: './src/assets/light-logo.png',
+      dark: './src/assets/dark-logo.png',
+    },
+    social: {
+      discord: 'https://discord.com/invite/vpeHDsg',
+      github: 'https://github.com/withastro/starlight'
+    },
+    editLink: {
+      baseUrl: 'https://github.com/ShokoAnime/ShokoDocs'
+    },
+    lastUpdated: true,
+    sidebar: [{
+      label: 'Getting Started',
+      // Autogenerate a group of links for the 'getting-started' directory.
+      autogenerate: {
+        directory: 'getting-started'
+      }
+    }],
+    components: {
+      TableOfContents: './src/components/TableOfContents.astro',
+      SiteTitle: './src/components/SiteTitle.astro'
+    },
+    customCss: [
+      './src/styles/custom.css',
+    ],
+  })]
 });
