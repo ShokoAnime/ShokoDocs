90dd235ab1a7c8ee3a9a8c4f698b4c3c871e8097
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-12-13 00:16:49 -0800

    Additional Work on Frame.

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,5 +3,4 @@ import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
