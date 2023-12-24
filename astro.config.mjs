e70af96d490845ff2e0cfb97d6c78544e4f28bfa
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2023-12-24 01:47:15 -0800

    Move Unchecked Files to _OLD, Update Images and Begin Checking Process.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,50 +9,55 @@ export default defineConfig({
       title: "ShokoDocs",
       logo: {
         light: "./src/assets/light-logo.png",
-        dark: "./src/assets/dark-logo.png",
+        dark: "./src/assets/dark-logo.png"
       },
       social: {
         discord: "https://discord.com/invite/vpeHDsg",
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/withastro/starlight"
       },
       editLink: {
-        baseUrl: "https://github.com/ShokoAnime/ShokoDocs",
+        baseUrl: "https://github.com/ShokoAnime/ShokoDocs"
       },
       lastUpdated: true,
       components: {
         TableOfContents: "./src/components/TableOfContents.astro",
-        SiteTitle: "./src/components/SiteTitle.astro",
+        SiteTitle: "./src/components/SiteTitle.astro"
       },
       sidebar: [
         {
           autogenerate: {
-            directory: "getting-started",
+            directory: "getting-started"
           },
-          label: "Getting Started",
+          label: "Getting Started"
         },
         {
           autogenerate: {
-            directory: "mediaserver-integration",
+            directory: "mediaserver-integration"
           },
-          label: "Mediaserver-Integration",
+          label: "Mediaserver-Integration"
         },
         {
           label: "Shoko Suite",
-          link: "shoko-suite",
+          link: "shoko-suite"
         },
         {
           label: "FAQ",
-          link: "faq",
+          link: "faq"
         },
         {
           autogenerate: {
-            directory: "changelog",
+            directory: "changelog"
           },
-          label: "Changelog",
-        },
+          label: "Changelog"
+        }
       ],
-      customCss: ["./src/styles/custom.css"],
+      customCss: [
+        './src/styles/custom.css',
+        '@fontsource/lexend-deca',
+        '@fontsource/lexend-deca/600.css'
+
+      ]
     }),
-    react(),
-  ],
+    react()
+  ]
 });
