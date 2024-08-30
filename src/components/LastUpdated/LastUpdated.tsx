caf26b071e88e1bbd063275eb759dde4f30a272e
ElementalCrisis <9443295+ElementalCrisis@users.noreply.github.com>
2024-08-30 16:58:47 -0700

    Fix LastUpdated EasyTable Component.

--- src/components/LastUpdated/LastUpdated.tsx
+++ src/components/LastUpdated/LastUpdated.tsx
@@ -3,22 +3,30 @@ import lastUpdated from '/public/files/last-updated.json';
 import EasyTable from '@components/EasyTable/EasyTable.tsx';
 
 const LastUpdated = () => {
-	const recentUpdates = lastUpdated
-		.slice(0, 10)
-		.map((data) => [
-			<a href={data.url}>{data.name}</a>,
-			data.location,
-			data.lastUpdated,
-			<a href={`https://github.com/ShokoAnime/ShokoDocs/commit/${data.commitId}`}>{data.commitId.slice(0, 6)}</a>,
-		]);
+  const recentUpdates = lastUpdated.slice(0, 10).map((data) => ({
+    Page: {
+      link: data.url,
+      title: data.name,
+    },
+    Location: data.location,
+    UpdatedOn: data.lastUpdated,
+    GitHubLink: {
+      link: `https://github.com/ShokoAnime/ShokoDocs/commit/${data.commitId}`,
+      title: data.commitId.slice(0, 6),
+    },
+  }));
 
-	return (
-		<EasyTable
-			header={['Page', 'Location', 'Updated On', 'GitHub Link']}
-			data={recentUpdates}
-			lineOnly={true}
-		/>
-	);
+  return (
+    <EasyTable
+      columns={[
+        { name: 'Page', header: 'Page' },
+        { name: 'Location', header: 'Location' },
+        { name: 'UpdatedOn', header: 'Updated On' },
+        { name: 'GitHubLink', header: 'GitHub Link' },
+      ]}
+      data={recentUpdates}
+    />
+  );
 };
 
 export default LastUpdated;
